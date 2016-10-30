8e422c867f25424fbc2d95e4a11cb76ea4de66ac
Jody LeCompte <jody@jodylecompte.com>
2016-10-30 06:18:43 -0500

    Create land.py

---
+++ objects/land.py
@@ -0,0 +1,71 @@
+{"rows":[
+	{"C": [
+		{"tile_start": "6C","tile_end": "21C", "side": NULL},
+		{"tile_start": "26C","tile_end": "57C", "side": NULL},
+	]},
+	{"D": [
+		{"tile_start": "20D","tile_end": "20D", "side": "USA"},
+		{"tile_start": "38D","tile_end": "42D", "side": "USA"},
+		{"tile_start": "44D","tile_end": "56D", "side": "USA"}
+	]},
+	{"E": [
+		{"tile_start": "3W","tile_end": "16W", "side": "USA"}
+	]},
+	{"F": [
+		{"tile_start": "3W","tile_end": "16W", "side": "USA"}
+	]},
+	{"G": [
+		{"tile_start": "3W","tile_end": "16W", "side": "USA"}
+	]},
+	{"H": [
+		{"tile_start": "3W","tile_end": "16W", "side": "USA"}
+	]},
+	{"I": [
+		{"tile_start": "3W","tile_end": "16W", "side": "USA"}
+	]},
+	{"J": [
+		{"tile_start": "3W","tile_end": "16W", "side": "USA"}
+	]},
+	{"K": [
+		{"tile_start": "3W","tile_end": "16W", "side": "USA"}
+	]},
+	{"L": [
+		{"tile_start": "3W","tile_end": "16W", "side": "USA"}
+	]},
+	{"M": [
+		{"tile_start": "3W","tile_end": "16W", "side": "USA"}
+	]},
+	{"N": [
+		{"tile_start": "3W","tile_end": "16W", "side": "USA"}
+	]},
+	{"O": [
+		{"tile_start": "3W","tile_end": "16W", "side": "USA"}
+	]},
+	{"P": [
+		{"tile_start": "3W","tile_end": "16W", "side": "USA"}
+	]},
+	{"Q": [
+		{"tile_start": "3W","tile_end": "16W", "side": "USA"}
+	]},
+	{"R": [
+		{"tile_start": "3W","tile_end": "16W", "side": "USA"}
+	]},
+	{"S": [
+		{"tile_start": "3W","tile_end": "16W", "side": "USA"}
+	]},
+	{"T": [
+		{"tile_start": "3W","tile_end": "16W", "side": "USA"}
+	]},
+	{"U": [
+		{"tile_start": "3W","tile_end": "16W", "side": "USA"}
+	]},
+	{"V": [
+		{"tile_start": "3W","tile_end": "16W", "side": "USA"}
+	]},
+	{"W": [
+		{"tile_start": "3W","tile_end": "16W", "side": "USA"}
+	]},
+	{"X": [
+		{"tile_start": "3W","tile_end": "16W", "side": "USA"}
+	]}
+]}
